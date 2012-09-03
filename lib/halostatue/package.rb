66f4e7fabe215e1c73b7d91c124e183ef088c6ae
Austin Ziegler <austin@surfeasy.com>
2012-09-03 12:48:04 -0400

    lib/halostatue/package/* is for packages only.
    - The package generator tasks are now part of package.rb.

--- lib/halostatue/package.rb
+++ lib/halostatue/package.rb
@@ -2,6 +2,7 @@
 
 require 'halostatue'
 require 'pathname'
+require 'erb'
 
 class Halostatue::Package
   include Rake::DSL
@@ -45,8 +46,6 @@ class Halostatue::Package
       package_methods = package.public_methods -
         Halostatue::Package.public_instance_methods
 
-      default_packages = []
-
       namespace :package do
         package_methods.each do |m|
           namespace m do
@@ -55,21 +54,21 @@ class Halostatue::Package
             d.gsub!(/\{\{name\}\}/, package.name)
             desc d unless d.empty? or private_package?
             depends = [ dependencies, packages_path ].flatten
-            task package_task =>depends do |t|
+            task package_task => depends do |t|
               package.send(m, t)
               package.update_package_list(m)
             end
           end
 
           desc "#{m.to_s.capitalize} all packages."
-          task m => 'package:#{m}:#{package_task}'
+          task m => "package:#{m}:#{package_task}"
         end
       end
 
-      unless default_packages.empty?
+      if default_package?
         namespace :package do
           desc "Install default packages."
-          task :install_defaults => default_packages
+          task :install_defaults => "package:install:#{package.name}"
         end
       end
     end
@@ -267,4 +266,115 @@ class Halostatue::Package
   end
 end
 
-require 'halostatue/package/generator'
+class Halostatue::Package::Generator
+  include Rake::DSL
+
+  class << self
+    include Rake::DSL
+
+    def inherited(subclass)
+      known_generators << subclass
+    end
+
+    def known_generators
+      @known_generators ||= []
+    end
+
+    def define_tasks(installer)
+      known_generators.each do |subclass|
+        generator = subclass.new(installer)
+
+        namespace :generate do
+          namespace :package do
+            desc generator.description if generator.description
+            task generator.task_name, [ :name, :url ] do |t, args|
+              generator.create(t, args)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  class << self
+    def name(package = nil)
+      @name = package if package
+      @name ||= self.to_s.split(/::/).last.downcase
+      @name &&= @name.downcase
+      @name
+    end
+
+    def description(text = nil)
+      @description = text if text
+      @description ||= "Generates a #{name}-style package"
+      @description
+    end
+  end
+
+  def initialize(installer)
+    @installer = installer
+  end
+
+  attr_reader :installer
+
+  def name
+    self.class.name
+  end
+
+  def task_name
+    name.to_sym
+  end
+
+  def description
+    self.class.description
+  end
+
+  def path(*args)
+    installer.source_file(*%W(lib halostatue package)).join(*args)
+  end
+
+  def create(t, args)
+    source_file = path("#{args.name.downcase}.rb")
+
+    raise "Package #{args.name} already exists." if source_file.exist?
+
+    klass_name = args.name.capitalize
+    url = args.url || ENV['url'] || '<url>'
+
+    tmpl = ERB.new(template, 0, '%<>')
+
+    File.open(source_file, "w") { |f| f.write(tmpl.result(binding)) }
+  end
+end
+
+class Halostatue::Package::Generator::Git < Halostatue::Package::Generator
+  def template
+    <<-EOS
+# -*- ruby encoding: utf-8 -*-
+
+require 'halostatue/package'
+
+class Halostatue::Package::<%= klass_name %> < Halostatue::Package
+  include Halostatue::Package::GitPackage
+
+  url "<%= url %>"
+end
+    EOS
+  end
+end
+
+class Halostatue::Package::Generator::Hg < Halostatue::Package::Generator
+  def template
+    <<-EOS
+# -*- ruby encoding: utf-8 -*-
+
+require 'halostatue/package'
+
+class Halostatue::Package::<%= klass_name %> < Halostatue::Package
+  include Halostatue::Package::HgPackage
+
+  url "<%= url %>"
+end
+    EOS
+  end
+end
